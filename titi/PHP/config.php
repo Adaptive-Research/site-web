cea6798459977f2ae5222f1a6a1a70bc55f0c04e
Adaptive-Research <ddupard@adaptive-research.eu>
2022-07-20 15:10:43 +0200

    v4

--- titi/PHP/config.php
+++ titi/PHP/config.php
@@ -15,43 +15,22 @@ $RepertoireData = "/data-dev/SEO" ;
 if ($_SERVER["SERVER_NAME"] == "plusdeca.test") 
 {
     $servername = "192.168.1.206" ;
-/*
-    $username = "dba";
-    $password = "dbaRohita;156";
-    $dbname = "SEO";
-*/
     $ServeurWeb = "http://78.249.128.56:8003" ; // serveurs Web de dev
-  
- //   $RepertoireData = "/data-dev/SEO" ;
-
 }
 
 
-/*
+
 // base de données de dev et repertoires data de dev
 if ($_SERVER["SERVER_NAME"] == "localhost") 
 {
-    $servername = $_SERVER["SERVER_ADDR"] ;
-    $username = "dba";
-    $password = "dbaRohita;156";
-    $dbname = "SEO";
-
+    $servername = "127.0.0.1" ;
     $ServeurWeb = "http://localhost" ;
-
     $RepertoireData = "/data-dev/SEO" ;
 }
-*/
 
 if ($_SERVER["SERVER_ADDR"] == "192.168.1.68" )
 {
-/*    
-    $servername = $_SERVER["SERVER_ADDR"] ;
-    $username = "dba";
-    $password = "dbaRohita;156";
-    $dbname = "SEO";
-*/
     $ServeurWeb = "http://seo.test" ;
-    //$RepertoireData = "/data-dev/SEO" ;
 }
 
 
@@ -62,7 +41,5 @@ $RG = $RepertoireData.'/RankingGoogle' ;
 $SW = $RepertoireData.'/SitesWeb' ;
 $PW = $RepertoireData.'/PagesWeb' ;
 
-
-#echo "<script>alert('".$servername." ".$username." ".$dbname."');</script>";
 
 ?>
